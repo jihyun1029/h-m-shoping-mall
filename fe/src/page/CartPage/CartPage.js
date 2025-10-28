2c4bdf49afafb0b149430bddd68176995b2b94b9
jihyun1029 <parkji2028@naver.com>
2025-10-28 18:17:41 +0900

    카트페이지 프론트엔드

--- fe/src/page/CartPage/CartPage.js
+++ fe/src/page/CartPage/CartPage.js
@@ -13,7 +13,8 @@ const CartPage = () => {
 
   useEffect(() => {
     //카트리스트 불러오기
-  }, []);
+    dispatch(getCartList());
+  }, [dispatch]);
 
   return (
     <Container>
@@ -31,7 +32,7 @@ const CartPage = () => {
           )}
         </Col>
         <Col xs={12} md={5}>
-          <OrderReceipt />
+          <OrderReceipt cartList = {cartList} totalPrice={totalPrice}/>
         </Col>
       </Row>
     </Container>
